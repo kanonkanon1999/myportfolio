2111f1d839034dcabf2cab332e73b4c5ffff4bc7
kanonkanon1999 <Konnomakoto713@icloud.com>
2020-06-22 12:29:21 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render()
+  {
+    return (
+    <div className='Container'>
+      <h1 className='Greet'>Hello React!</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
